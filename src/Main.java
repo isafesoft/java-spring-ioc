0b1eef6d43eb38784d5a49c867c482bb1c508f32
Mark Xu <isafesoft@hotmail.com>
2018-12-12 14:03:11 -0500

    java configuration by java class instead of xml

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 import com.mk.springdemo.Coach;
 import com.mk.springdemo.CricketCoach;
+import com.mk.springdemo.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        //ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(ProjectConfig.class);
 
         Coach theCoach = context.getBean("tennisCoach", Coach.class);
         System.out.println(theCoach.getDailyFortune());
---
+++ src/com/mk/springdemo/ProjectConfig.java
@@ -0,0 +1,9 @@
+package com.mk.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.mk.springdemo")
+public class ProjectConfig {
+}
